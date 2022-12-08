be383b690cbc73bc310477455377c22901c2dfd7
gyojincompany <luckyred@gmail.com>
2022-12-08 09:35:37 +0900

    DB설정 및 보안초기설정 완료

---
+++ src/main/java/com/gyojincompany/home/MemberConTest20221208Application.java
@@ -0,0 +1,13 @@
+package com.gyojincompany.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberConTest20221208Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberConTest20221208Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gyojincompany/home/MemberConTest20221208ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyojincompany.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberConTest20221208ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
